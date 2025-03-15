4b1982a6c328ffefbad995de1b52c0cf31d01478
Roma <email@email.ru>
2025-03-15 17:57:55 +0300

    первая версия

---
+++ game.py
@@ -0,0 +1,41 @@
+print("hello world")
+#from PIL import Image
+#img = Image.open("pizza.jpg")
+#img.show()
+import tkinter as tk
+root = tk.Tk()
+canvas = tk.Canvas(root, width=800, height=600, bg="white")
+canvas.pack()
+image = tk.PhotoImage(file="pizza.png")
+
+
+def on_press(event):
+    global start_x, start_y
+    start_x, start_y = event.x, event.y
+
+def on_drag(event):
+    global start_x, start_y
+    dx, dy = event.x - start_x, event.y - start_y
+    #canvas.move(image_id, dx, dy)
+    start_x, start_y = event.x, event.y  
+
+canvas.bind("<ButtonPress-1>", on_press)
+canvas.bind("<B1-Motion>", on_drag)
+start_x, start_y = 0, 0
+
+
+def update():
+    #print("next frame")
+    canvas.create_rectangle(0, 0, 800, 600, fill="white")
+    canvas.create_rectangle(622, 360, 630, 148, fill="purple")
+    canvas.create_rectangle(3200, 360, 600, 1408, fill="blue")
+    canvas.create_rectangle(542, 366, 606, 148, fill="gray")
+    canvas.create_rectangle(542, 366, 606, 148, fill="brown")
+    canvas.create_oval(100,100,210,210, fill="green")
+    canvas.create_image(start_x, start_y, anchor=tk.NW, image=image)
+    root.after(16, update)  
+
+root.after(16, update)
+
+root.mainloop()
+#input()
