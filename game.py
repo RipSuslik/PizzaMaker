85fc0191f2ae34ca4f3fd2a7ec5c17e6e85847ba
Roma <email@email.ru>
2025-03-18 15:28:23 +0300

    mikser and pizza yeaaaaa

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.init()
 mikser_image = pygame.image.load('mikser.png')
+pizza_image = pygame.image.load('pizza.png')
 screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("PizzaMaker")
 mouse_x, mouse_y = 0, 0
@@ -13,6 +14,7 @@ while running:
             mouse_x, mouse_y = pygame.mouse.get_pos()
     screen.fill((255, 255, 255))  # Clear background
     screen.blit(mikser_image, (mouse_x, mouse_y))  # Draw mikser image
+    screen.blit(pizza_image, (mouse_x, mouse_y))  # Draw pizza image
     pygame.display.flip()  # Update display
 pygame.quit()
 
