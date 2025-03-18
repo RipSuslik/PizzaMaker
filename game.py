4ca851956a3c4045738768a35c722d17c73d4d2c
Roma <email@email.ru>
2025-03-18 15:25:42 +0300

    mikser added

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-pizza_image = pygame.image.load('pizza.png')
+mikser_image = pygame.image.load('mikser.png')
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("PizzaMaker")
 mouse_x, mouse_y = 0, 0
@@ -12,7 +12,7 @@ while running:
         elif event.type == pygame.MOUSEMOTION:
             mouse_x, mouse_y = pygame.mouse.get_pos()
     screen.fill((255, 255, 255))  # Clear background
-    screen.blit(pizza_image, (mouse_x, mouse_y))  # Draw pizza image
+    screen.blit(mikser_image, (mouse_x, mouse_y))  # Draw mikser image
     pygame.display.flip()  # Update display
 pygame.quit()
 
