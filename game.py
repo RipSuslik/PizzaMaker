32ddc499f0e93b83e3b16d1e715c2a4ca24b8c3f
Roma <email@email.ru>
2025-03-22 12:51:50 +0300

    пицца двигается а миксер нет

--- game.py
+++ game.py
@@ -5,16 +5,24 @@ pizza_image = pygame.image.load('pizza.png')
 screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("PizzaMaker")
 mouse_x, mouse_y = 0, 0
+pizza_x, pizza_y = 450, 700
 running = True
+mouse_down = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEMOTION:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_down = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouse_down = False
     screen.fill((255, 255, 255))  # Clear background
-    screen.blit(mikser_image, (mouse_x, mouse_y))  # Draw mikser image
-    screen.blit(pizza_image, (mouse_x, mouse_y))  # Draw pizza image
+    screen.blit(mikser_image, (450, 550))  # Draw mikser image
+    if mouse_down:
+        pizza_x, pizza_y = mouse_x, mouse_y     
+    screen.blit(pizza_image, (pizza_x, pizza_y))  # Draw pizza image
     pygame.display.flip()  # Update display
 pygame.quit()
 
