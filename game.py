eabbcf7ccbab4ced90ee80ee6a245a01a3709d65
Roma <email@email.ru>
2025-03-26 11:56:15 +0300

    something goes wrong and idk what

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ import pygame
 pygame.init()
 mikser_image = pygame.image.load('mikser.png')
 pizza_image = pygame.image.load('pizza.png')
+cheeese_image = pygame.image.load('cheeese.png')
+cheese_image = pygame.image.load('cheese.png')
+tomatoes_image = pygame.image.load('tomatoes.png')
 screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("PizzaMaker")
 mouse_x, mouse_y = 0, 0
@@ -23,6 +26,13 @@ while running:
     if mouse_down:
         pizza_x, pizza_y = mouse_x, mouse_y     
     screen.blit(pizza_image, (pizza_x, pizza_y))  # Draw pizza image
+    pizza_x, pizza_y = mouse_x, mouse_y     
+    screen.blit(cheeese_image, (cheeese_x, cheeese_y))  # Draw cheeese image
+    cheeese_x, cheeese_y = mouse_x, mouse_y     
+    screen.blit(cheese_image, (cheese_x, cheese_y))  # Draw cheese image
+    cheese_x, cheese_y = mouse_x, mouse_y
+    screen.blit(tomatoes_image, (tomatoes_x, tomatoes_y))  # Draw tomatoes image
+    tomatoes_x, tomatoes_y = mouse_x, mouse_y
     pygame.display.flip()  # Update display
 pygame.quit()
 
