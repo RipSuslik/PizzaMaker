f7f0dc426e74e3bfa7bb3797a1eda5b8d9200898
Roma <email@email.ru>
2025-04-05 19:43:12 +0300

    чет много кода ну ладно

--- game.py
+++ game.py
@@ -1,12 +1,30 @@
 import pygame
+
+def function ():
+    print ("hello from function")
+    print ("hello from function1")
+    print ("hello from function2")
+    print ("hello from function3")
+
 pygame.init()
+
+images_names = ['mikser.png','pizza.png','cheeese.png','cheese.png','tomatoes.png']
+images = []
+i=0
+while (i<5):
+    name = images_names[i]
+    image = pygame.image.load(name)
+    images.append(image)
+    i=i+1
 mikser_image = pygame.image.load('mikser.png')
 pizza_image = pygame.image.load('pizza.png')
 cheeese_image = pygame.image.load('cheeese.png')
 cheese_image = pygame.image.load('cheese.png')
 tomatoes_image = pygame.image.load('tomatoes.png')
+
 screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("PizzaMaker")
+
 mouse_x, mouse_y = 0, 0
 pizza_x, pizza_y = 450, 700
 cheeese_x, cheeese_y = 450, 700
@@ -14,6 +32,7 @@ cheese_x, cheese_y = 450, 700
 tomatoes_x, tomatoes_y = 450, 700
 running = True
 mouse_down = False
+#function()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,10 +43,13 @@ while running:
             mouse_down = True
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_down = False
-    screen.fill((255, 255, 255))  # Clear background
-    screen.blit(mikser_image, (450, 550))  # Draw mikser image
+
     if mouse_down:
-        pizza_x, pizza_y = mouse_x, mouse_y     
+        pizza_x, pizza_y = mouse_x, mouse_y
+
+    screen.fill((255, 255, 255))  # Clear background
+
+    screen.blit(mikser_image, (450, 550))  # Draw mikser image     
     screen.blit(pizza_image, (pizza_x, pizza_y))  # Draw pizza image
     pizza_x, pizza_y = mouse_x, mouse_y     
     screen.blit(cheeese_image, (cheeese_x, cheeese_y))  # Draw cheeese image
@@ -36,7 +58,7 @@ while running:
     cheese_x, cheese_y = mouse_x, mouse_y
     screen.blit(tomatoes_image, (tomatoes_x, tomatoes_y))  # Draw tomatoes image
     tomatoes_x, tomatoes_y = mouse_x, mouse_y
+
     pygame.display.flip()  # Update display
 pygame.quit()
-
 
