470cf8eb86e42046e91dd2859fd0065e5f6ed5da
RipSuslik <tarrantella78@gmail.com>
2025-04-20 12:05:06 +0300

    можно двигать вещи с помощью клавиш

--- game.py
+++ game.py
@@ -22,14 +22,20 @@ screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("PizzaMaker")
 
 mouse_x, mouse_y = 0, 0
-pizza_x, pizza_y = 450, 700
-cheeese_x, cheeese_y = 450, 700
+pizza_x, pizza_y = 100, 700
+cheeese_x, cheeese_y = 150, 700
 cheese_x, cheese_y = 450, 700
-tomatoes_x, tomatoes_y = 450, 700
+tomatoes_x, tomatoes_y = 1050, 700
 running = True
 mouse_down = False
 
-while running:
+pizza_selected = False
+cheeese_selected = False
+cheese_selected = False
+tomatoes_selected = False
+
+while running: 
+    #получаем информацию с помощью get() от pygame
     events = pygame.event.get() #сохраняем в евентс события из pygame
     for current_event in events:
         if current_event.type == pygame.QUIT:
@@ -40,6 +46,22 @@ while running:
             mouse_down = True
         elif current_event.type == pygame.MOUSEBUTTONUP:
             mouse_down = False
+        elif current_event.type == pygame.KEYDOWN:
+            
+            pizza_selected = False
+            cheeese_selected = False
+            cheese_selected = False
+            tomatoes_selected = False
+
+            if current_event.key == pygame.K_1:
+                pizza_selected = True
+            elif current_event.key == pygame.K_2:
+                cheeese_selected = True
+            elif current_event.key == pygame.K_3:
+                cheese_selected = True
+            elif current_event.key == pygame.K_4:
+                tomatoes_selected = True
+
             """ elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 print("Left arrow pressed")
@@ -50,12 +72,18 @@ while running:
             else:
                 print(f"Key {event.key} pressed")"""
 
+    #обрабатываем информацию 
     if mouse_down:
-        pizza_x, pizza_y = mouse_x, mouse_y
-        cheeese_x, cheeese_y = mouse_x, mouse_y
-        cheese_x, cheese_y = mouse_x, mouse_y
-        tomatoes_x, tomatoes_y = mouse_x, mouse_y
+        if pizza_selected: 
+            pizza_x, pizza_y = mouse_x, mouse_y
+        if cheeese_selected:
+            cheeese_x, cheeese_y = mouse_x, mouse_y
+        if cheese_selected:
+            cheese_x, cheese_y = mouse_x, mouse_y
+        if tomatoes_selected:
+            tomatoes_x, tomatoes_y = mouse_x, mouse_y
 
+    #совершаем действия с игрой (картинками (пока что))
     screen.fill((255, 255, 255))  # Clear background
 
     screen.blit(mikser_image, (450, 550))  # Draw mikser image     
