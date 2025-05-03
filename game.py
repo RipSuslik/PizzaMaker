9a44376896d8ad86c16db4278b6f7428b924a02e
RipSuslik <tarrantella78@gmail.com>
2025-05-03 11:45:16 +0300

    добавили список объектов И изменили пиццу на обьект 1 и т.д

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
+from dataclasses import dataclass
 import pygame
 
 pygame.init()
 
+@dataclass
+class Object:
+    image: any
+    pos: tuple[int,int]  
+    selected: bool
+    under_mouse: bool
 """
 images_names = ['mikser.png','pizza.png','cheeese.png','cheese.png','tomatoes.png']
 images = []
@@ -12,29 +19,37 @@ while (i<5):
     images.append(image)
     i=i+1
 """ 
-mikser_image = pygame.image.load('mikser.png')
+objects = [
+    Object(pygame.image.load('mikser.png'),(450,550), False, False),
+    Object(pygame.image.load('pizza.png'),(500,50), False, False),
+    Object(pygame.image.load('cheeese.png'),(15,0), False, False),
+    Object(pygame.image.load('cheese.png'),(0,300), False, False),
+    Object(pygame.image.load('tomatoes.png'),(30,600), False, False)
+]
+
+"""mikser_image = pygame.image.load('mikser.png')
 pizza_image = pygame.image.load('pizza.png')
 cheeese_image = pygame.image.load('cheeese.png')
 cheese_image = pygame.image.load('cheese.png')
 tomatoes_image = pygame.image.load('tomatoes.png')
-
+"""
 screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("PizzaMaker")
 
 mouse_x, mouse_y = 0, 0
-pizza_x, pizza_y = 100, 700
-cheeese_x, cheeese_y = 150, 700
-cheese_x, cheese_y = 450, 700
-tomatoes_x, tomatoes_y = 1050, 700
+"""pizza_x, pizza_y = 500, 0
+cheeese_x, cheeese_y = 15, 0
+cheese_x, cheese_y = 0, 300 
+tomatoes_x, tomatoes_y = 30, 600"""
 running = True
 mouse_down = False
 
-pizza_under_mouse = False
+"""pizza_under_mouse = False
 
 pizza_selected = False
 cheeese_selected = False
 cheese_selected = False
-tomatoes_selected = False
+tomatoes_selected = False"""
 
 while running: 
     #получаем информацию с помощью get() от pygame
@@ -44,9 +59,9 @@ while running:
             running = False
         elif current_event.type == pygame.MOUSEMOTION:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            pizza_under_mouse = False
-            if mouse_x > pizza_x and mouse_y > pizza_y and mouse_x < pizza_x + pizza_image.get_width() and mouse_y < pizza_y + pizza_image.get_height():
-                pizza_under_mouse = True    
+            objects[1].under_mouse = False
+            if mouse_x > objects[1].pos[0] and mouse_y > objects[1].pos[1] and mouse_x < objects[1].pos[0] + objects[1].image.get_width() and mouse_y < objects[1].pos[1] + objects[1].image.get_height():
+                objects[1].under_mouse = True    
         elif current_event.type == pygame.MOUSEBUTTONDOWN:
             mouse_down = True
         elif current_event.type == pygame.MOUSEBUTTONUP:
@@ -71,7 +86,7 @@ while running:
             # если нажата клавиша мыши то выбираем объект под ней
     #обрабатываем информацию 
 
-    if mouse_down:
+    """if mouse_down: ToDo
         if pizza_selected: 
             pizza_x, pizza_y = mouse_x, mouse_y
         if cheeese_selected:
@@ -79,33 +94,44 @@ while running:
         if cheese_selected:
             cheese_x, cheese_y = mouse_x, mouse_y
         if tomatoes_selected:
-            tomatoes_x, tomatoes_y = mouse_x, mouse_y
+            tomatoes_x, tomatoes_y = mouse_x, mouse_y"""
 
     #совершаем действия с игрой (картинками (пока что))
     screen.fill((255, 255, 255))  # Clear background
 
-    screen.blit(mikser_image, (450, 550))  # Draw mikser image     
-    screen.blit(pizza_image, (pizza_x, pizza_y))  # Draw pizza image     
-    screen.blit(cheeese_image, (cheeese_x, cheeese_y))  # Draw cheeese image     
-    screen.blit(cheese_image, (cheese_x, cheese_y))  # Draw cheese image
-    screen.blit(tomatoes_image, (tomatoes_x, tomatoes_y))  # Draw tomatoes image
+    for obj in objects:
+        screen.blit(obj.image, obj.pos)
+
+
+    """screen.blit(objects[0].image, objects[0].pos)  # Draw mikser image     
+    screen.blit(objects[1].image, objects[1].pos)  # Draw pizza image     
+    screen.blit(objects[2].image, objects[2].pos)  # Draw cheeese image     
+    screen.blit(objects[3].image, objects[3].pos)  # Draw cheese image
+    screen.blit(objects[4].image, objects[4].pos)  # Draw tomatoes image"""
 
     icon = None
-    if pizza_selected:
-        icon = pygame.transform.scale(pizza_image,(100,100)) 
-    if cheeese_selected:
-        icon = pygame.transform.scale(cheeese_image,(100,100)) 
-    if cheese_selected:
-        icon = pygame.transform.scale(cheese_image,(100,100)) 
-    if tomatoes_selected:
-        icon = pygame.transform.scale(tomatoes_image,(100,100)) 
+    if objects[1].selected:
+        icon = pygame.transform.scale(objects[1].image,(100,100)) 
+    if objects[2].selected:
+        icon = pygame.transform.scale(objects[2].image,(100,100)) 
+    if objects[3].selected:
+        icon = pygame.transform.scale(objects[3].image,(100,100)) 
+    if objects[4].selected:
+        icon = pygame.transform.scale(objects[4].image,(100,100)) 
 
     if icon != None:
         screen.blit(icon, (50,50,100,100))
     pygame.draw.rect(screen,(255,255,0),(50,50,100,100),3)
 
-    if pizza_under_mouse:
-        pygame.draw.rect(screen,(245,52,134),(pizza_x,pizza_y,pizza_image.get_width(),pizza_image.get_height()),3)
+    if objects[1].under_mouse:
+        pygame.draw.rect(
+            screen,
+            (245,52,134),
+            (objects[1].pos[0],
+             objects[1].pos[1],
+             objects[1].image.get_width(),
+             objects[1].image.get_height()),
+            3)
 
     pygame.display.flip()  # Update display
 pygame.quit()
