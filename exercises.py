8e9f8177512616994d2817287b07eab731290e20
RipSuslik <tarrantella78@gmail.com>
2025-06-07 10:55:40 +0300

    добавили упражнения

---
+++ exercises.py
@@ -0,0 +1,47 @@
+print("hello world") #pechatayem
+
+#переменные
+
+korobka = 5 
+
+NeCorobka = True
+
+ChtoTo = "stroka" 
+
+EschheChtoTo = 'gegegegege'
+
+#вывод переменных
+
+print (korobka, NeCorobka, ChtoTo, EschheChtoTo)
+
+#Calculator
+
+Neznay = 10
+
+Znay = 12
+
+Otvet = Neznay + Znay
+
+print(Otvet)
+
+#Ввод строчек
+
+print("How is your name")
+
+Name = input()
+
+print("Hello", Name)
+
+#Ввод чисел
+
+text_A = input("введите число А: ")
+
+text_B = input("введите число Б: ")
+
+A = int(text_A)
+
+B = int(text_B)
+
+Result = A + B
+
+print("Сумма равна: ", Result)
